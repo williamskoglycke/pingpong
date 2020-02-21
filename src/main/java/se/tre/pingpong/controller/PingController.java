274543b29df83546da7532eb7619ebb083382c83
William Skoglycke <William.Skoglycke@tre.se>
2020-02-21 08:41:08 +0100

    Added response entity

--- src/main/java/se/tre/pingpong/controller/PingController.java
+++ src/main/java/se/tre/pingpong/controller/PingController.java
@@ -2,6 +2,7 @@ package se.tre.pingpong.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +17,18 @@ public class PingController {
             path = "/ping",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public String ping() {
+    public ResponseEntity<String> ping() {
         log.info("ping called");
-        return "pong";
+        return ResponseEntity.ok().body("pong");
     }
 
     @GetMapping(
             path = "/echo/{echo}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public String echo(@PathVariable final String echo) {
+    public ResponseEntity<String> echo(@PathVariable final String echo) {
         log.info("echo called with parameters");
-        return echo;
+        return ResponseEntity.ok().body(echo);
     }
 
 }
